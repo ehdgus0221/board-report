02adb2f6d434cfdd2bce361558a52dc8ade35e5e
DongHyeon Lim <dlaehdgus23@naver.com>
2025-06-22 15:55:25 +0900

    feat : 게시글 조회 DTO 작성

--- src/main/java/com/springreport/springreport/post/dto/PostDto.java
+++ src/main/java/com/springreport/springreport/post/dto/PostDto.java
@@ -1,35 +1,36 @@
 package com.springreport.springreport.post.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.springreport.springreport.member.dto.MemberDto;
 import com.springreport.springreport.post.domain.Post;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
+@Getter
 public class PostDto {
-/*    @ToString
-    @Setter
-    public static class Request {
-        private String title;
-        private String description;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
-        private LocalDateTime updDate;
-        private String writeUserId;
+    private Long id;
+    private String subject;
+    private String contents;
+    private MemberDto memberDto;
 
-    }*/
-    public record Request(
-            String subject,
-            String contents,
-            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
-            LocalDateTime updDate,
-            String password
-    ) {
-        // 엔티티 객체 생성을 쉽게 하기 위함
-        public Post toEntity() {
-            return Post.builder()
-                    .subject(subject)
-                    .contents(contents)
-                    .password(password)
-                    .build();
-        }
+    @Builder(access = AccessLevel.PRIVATE)
+    public PostDto(Long id, String subject, String contents, MemberDto memberDto) {
+        this.id = id;
+        this.subject = subject;
+        this.contents = contents;
+        this.memberDto = memberDto;
+    }
+
+    @Builder(access = AccessLevel.PRIVATE)
+    public static PostDto fromEntity(Post post) {
+        PostDto postDto = PostDto.builder()
+                .id(post.getId())
+                .subject(post.getSubject())
+                .contents(post.getContents())
+                .memberDto(MemberDto.fromEntity(post.getMember()))
+                .build();
+        return postDto;
     }
 }
