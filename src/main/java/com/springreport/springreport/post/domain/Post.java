afb369811c9f4ab6efe70ef8c42b831125aace0c
DongHyeon Lim <dlaehdgus23@naver.com>
2025-06-22 14:49:11 +0900

    feat : 게시글 entity 수정

--- src/main/java/com/springreport/springreport/post/domain/Post.java
+++ src/main/java/com/springreport/springreport/post/domain/Post.java
@@ -4,25 +4,33 @@ import com.springreport.springreport.post.enums.PostStatus;
 import com.springreport.springreport.member.Member;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
+@SQLDelete(sql = "UPDATE post SET status = off WHERE post_id = ?")
+@Where(clause = "is_deleted = false")
+
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id")
     private Long id;
-    @Setter
-    private String title;
-    @Setter
-    private String description;
-    @Setter
-    private String writeUser;
+
+    private String subject;
+
+    @Lob
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
     @Enumerated(EnumType.STRING)
     @Setter
     private PostStatus status;
@@ -33,26 +41,24 @@ public class Post {
     @Setter
     private LocalDateTime regDate;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    @Setter
-    private Member member;
-    @Setter
-    private String writeUserPassword;
-
     @Builder
     public Post(
-            String title,
-            String description,
-            LocalDateTime updDate,
-            String writeUser,
-            String writeUserPassword
+            String subject,
+            String contents,
+            Member member
     ) {
-        this.title = title;
-        this.description = description;
-        this.updDate = updDate;
-        this.writeUser = writeUser;
-        this.writeUserPassword = writeUserPassword;
+        this.subject = subject;
+        this.contents = contents;
+        this.member = member;
+    }
+
+    public static Post of(String contents, String subject, Member member) {
+        Post post = Post.builder()
+                .subject(subject)
+                .contents(contents)
+                .member(member)
+                .build();
+        return post;
     }
 
     public void opening() {
